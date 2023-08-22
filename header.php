0143b581366ac98919031827ddb63375e93aa843
Agossou Innocent <agoss737@gmail.com>
2023-08-22 22:11:29 +0200

    Envoi du projet

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<!-- Cod?e par Coding Team-->
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Realtime Chat App | Coding Team</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="font/css/all.min.css"/>
+</head>
